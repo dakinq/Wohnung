83f42bbc4decf3874ee91485556477d1cef06be4
dakinq <tsdwhv5dg8@privaterelay.appleid.com>
2026-07-21 00:42:57 +0200

    Delete src directory

--- src/pages/Buchungen.jsx
+++
@@ -1,8 +0,0 @@
-export default function Buchungen() {
-  return (
-    <div className="page pt-4">
-      <h1>Buchungen</h1>
-      <p className="text-muted mt-1">Kommt in Phase 2.</p>
-    </div>
-  );
-}
